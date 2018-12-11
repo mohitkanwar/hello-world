1b13dae862a7a4734ae0abdd7e5134b16b51724a
Mohit Kanwar <m.m.kanwar@gmail.com>
2018-12-11 15:37:42 +0530

    added variable for name

--- src/main/java/com/mk/helloworld/helloworld/HelloWorldController.java
+++ src/main/java/com/mk/helloworld/helloworld/HelloWorldController.java
@@ -1,12 +1,13 @@
 package com.mk.helloworld.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
     @GetMapping("/hello")
-    public String getGreeting(){
-        return "Hello World";
+    public String getGreeting(@RequestParam(name = "name",required = false ,defaultValue = "World" )String name){
+        return "Hello "+name+"!";
     }
 }
